bf346dc3ff8cda367cc91f6a0973cb180912c74a
JasperEssien2 <jasperessien2@gmail.com>
2022-11-15 11:17:56 +0100

    ffi implementation macos

---
+++ macos/Runner/file_api.cpp
@@ -0,0 +1,8 @@
+//
+//  file_api.cpp
+//  Runner
+//
+//  Created by Jahswill Essien on 14/11/2022.
+//
+
+#include "file_api.hpp"
---
+++ macos/Runner/file_api.hpp
@@ -0,0 +1,13 @@
+//
+//  file_api.hpp
+//  Runner
+//
+//  Created by Jahswill Essien on 14/11/2022.
+//
+
+#ifndef file_api_hpp
+#define file_api_hpp
+
+#include <stdio.h>
+
+#endif /* file_api_hpp */
