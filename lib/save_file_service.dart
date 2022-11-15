1ddbdf5f99a76d192b0ed0ecf3e4def5cf08a21b
JasperEssien2 <jasperessien2@gmail.com>
2022-11-15 11:21:12 +0100

    changed service to return FutureOr not Future

--- lib/save_file_service.dart
+++ lib/save_file_service.dart
@@ -1,14 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/services.dart';
 
 abstract class SaveFileService {
-  Future<bool> saveFile({required String fileName, required String text});
+  FutureOr<bool> saveFile({required String fileName, required String text});
 }
 
 class MethodChannelService implements SaveFileService {
   //TODO1: Create a method channel
 
   @override
-  Future<bool> saveFile(
+  FutureOr<bool> saveFile(
       {required String fileName, required String text}) async {
     //TODO2: Invoke a method, passing in neccessary arguments
 
